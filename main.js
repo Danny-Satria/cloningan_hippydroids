f7c645febb50fb776fb00739785ed075ed4ee104
Danny Satria <danny.satriaku@gmail.com>
2023-11-07 23:39:40 +0700

    add function on javascript

---
+++ main.js
@@ -0,0 +1,35 @@
+function discord() {
+  window.location.href = "https://discord.gg/CktxZaDU";
+}
+
+function youtube() {
+  window.location.href = "https://www.youtube.com/@Hippydroid";
+}
+
+function tiktok() {
+  window.location.href =
+    "https://www.tiktok.com/@hippydroids?_t=8g1ywCkHhbD&_r=1";
+}
+
+function support() {
+  window.location.href = "https://saweria.co/hippydroids";
+}
+
+function trakteer() {
+  window.location.href = "https://trakteer.id/hippydroids";
+}
+
+function instagram() {
+  window.location.href =
+    "https://instagram.com/hippydroid?igshid=MzRlODBiNWFlZA==";
+}
+
+function twitter() {
+  window.location.href =
+    "https://x.com/Hippydroid?t=1kNtvDMHmU8tSnSanJpkdg&s=09";
+}
+
+function spotify() {
+  window.location.href =
+    "https://open.spotify.com/user/21noi4ztuxtr7ua4o6h5zy47q?si=fcb9615f1431416f&nd=1";
+}
